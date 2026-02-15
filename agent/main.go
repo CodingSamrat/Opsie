c4174e65bca4968240fc83431f5c3179105b5ed5
sam <samratbiswas@zohomail.in>
2026-02-16 02:57:21 +0530

    setup dev env using docker

---
+++ agent/main.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	// This is the main entry point for the agent.
+	// It will start the agent and listen for commands from the control panel.
+	// The agent will also send status updates to the control panel.
+}
