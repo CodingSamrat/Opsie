463d584cac1d40a3cd54ed8a49cdef1e6c1efa45
sam <samratbiswas@zohomail.in>
2026-03-05 12:42:35 +0530

    middlewares

---
+++ server/src/middlewares/index.ts
@@ -0,0 +1,15 @@
+// =================================================================================
+// Name:      : middlewares/index.ts
+// Author     : Sam
+// Description: Centralized export of all middleware modules.
+// =================================================================================
+
+
+
+/// Centralized middleware registry
+const XMW = {
+
+
+} as const;
+
+export default XMW;
